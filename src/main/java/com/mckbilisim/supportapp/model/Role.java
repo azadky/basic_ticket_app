999728d7fce69ac78f84991ab4df0a470307d449
azadky <azadky@gmail.com>
2025-05-14 15:34:23 +0300

    Project finished

---
+++ src/main/java/com/mckbilisim/supportapp/model/Role.java
@@ -0,0 +1,25 @@
+package com.mckbilisim.supportapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @Column(name = "users")
+    private List<User> users;
+
+}
